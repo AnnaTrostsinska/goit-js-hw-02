70ac2e7fb669ab4dc307e688c083d0d5d2809094
AnnaTrostsinska <146640649+AnnaTrostsinska@users.noreply.github.com>
2024-02-21 23:00:26 +0100

    hw 2

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,33 @@
-function getElementWidth (content, padding, border) {
-    
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
+function checkForSpam(message) {
 
-    const totaltWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-    return totaltWidth;
+    const lowerCaseMessage = message.toLowerCase();
+    switch(message) {
+        case lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale"):
+            return true;
+            break;
+        default:
+            return false;
+    }
 }
+    
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
+
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
+
+
+
+const blw1 = "spam"
+const blw2 = "sale"
+const string1 = "Latest technology news"
